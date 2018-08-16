30112de1052194271cad9421d012687f68fb52d2
rodrigues-daniel <daniel.rodrighes@gmail.com>
2018-08-16 17:54:16 -0300

    configurado bibliotecas ldap

--- kerberos/settings.py
+++ kerberos/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -80,10 +82,6 @@ try:
     from kerberos import local_settings
 except ImportError:
     pass
-
-
-
-
 
 
 DATABASES = {
