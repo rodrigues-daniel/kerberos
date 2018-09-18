c9fede542bde131e6b9e0f54fe2db83f24960b14
sudcl <sudcl@prd-dcl01.tce.govrn>
2018-09-18 18:45:24 -0300

    novas configuracoes

--- kerberosadm/apps.py
+++ kerberosadm/apps.py
@@ -1,7 +1,10 @@
+# -*- Coding: UTF-8 -*-
+#coding: utf-8
+
 from django.apps import AppConfig
 
 
 class KerberosadmConfig(AppConfig):
     name = 'kerberosadm'
-    verbose_name = 'Visualizações'
+    verbose_name = 'Visualizacoes'
 
