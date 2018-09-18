5aad59e844a7a579248e93836b529e111d586a52
sudcl <sudcl@prd-dcl01.tce.govrn>
2018-09-18 19:09:48 -0300

    novos arquivos

--- kerberos/settings.py
+++ kerberos/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kerberos.settings")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
