bdd07866bd8a8a2b3e5c648efeea131ec074a5cd
sudcl <sudcl@prd-dcl01.tce.govrn>
2018-09-19 18:34:25 -0300

    reconfigurado

--- kerberos/settings.py
+++ kerberos/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+from .  import local_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = 'hq4#1e&7@6ql1b$649r3$!t8efzjg31=ua#dt3z!1brajk3bsk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [local_settings.PROD]
 
 
 
@@ -93,7 +94,6 @@ WSGI_APPLICATION = 'kerberos.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 
-from .  import local_settings
 
 
 DATABASES = {
