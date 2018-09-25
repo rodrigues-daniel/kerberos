ce51fbf7fab6055fc27377ba5c3e25d0840c7595
sudcl <sudcl@prd-dcl01.tce.govrn>
2018-09-25 16:03:20 -0300

    m

--- kerberos/settings.py
+++ kerberos/settings.py
@@ -203,5 +203,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR,'/var/www/html/admin')
+STATIC_URL = '/var/www/html/'
